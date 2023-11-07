eb50f4131140bdcd5a506f4755f7c4a808b30155
TerfyHorizon <51277865+TerfyHorizon@users.noreply.github.com>
2023-11-07 20:24:14 +0000

    Added Data & Print for James

---
+++ python.py
@@ -0,0 +1,11 @@
+#Data for James
+name = "James"
+balance = 5000
+salary = 1200
+new_salary = 1800
+New_salary = salary + new_salary
+new_balance = balance + New_salary
+
+#Display Name & new_balance
+print(name)
+print(new_balance) 
